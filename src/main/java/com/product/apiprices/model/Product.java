791b1b66da4a16166513e10942ce78741d785d9d
rojumacue <juliethmartinez839@gmail.com>
2023-10-15 21:52:48 -0500

    add JPA product

---
+++ src/main/java/com/product/apiprices/model/Product.java
@@ -0,0 +1,50 @@
+package com.product.apiprices.model;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", length = 100, nullable = false)
+    private String name;
+    @Column(name = "amount", nullable = false)
+    private int amount;
+
+    @ManyToOne
+    @JoinColumn(name="BRAND_ID")
+    private Brand brand;
+
+    @OneToMany(mappedBy = "product")
+    private Set<Prices> prices;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+
+}
