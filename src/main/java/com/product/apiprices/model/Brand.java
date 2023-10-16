9de7a9c75f25020468cb516d359b205745c21686
rojumacue <juliethmartinez839@gmail.com>
2023-10-15 21:52:08 -0500

    add JPA Brand

---
+++ src/main/java/com/product/apiprices/model/Brand.java
@@ -0,0 +1,42 @@
+package com.product.apiprices.model;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "brand")
+public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", length = 100, nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "brand")
+    private Set<Product> products;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
+}
